1adec16c4dbfd7ad539d451e4347b672c43a578a
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2015-01-08 17:32:29 +0100

    Transfer Queue's Birth

---
+++ src/main/java/fr/epsi/controller/transfer/queue/TransferQueueController.java
@@ -0,0 +1,14 @@
+package fr.epsi.controller.transfer.queue;
+
+import fr.epsi.widgets.transfer.TransferQueue;
+import javafx.scene.layout.VBox;
+
+public class TransferQueueController {
+
+    public VBox transferQueueContainer;
+    public TransferQueue transferQueue;
+
+    public void initialize() {
+
+    }
+}
---
+++ src/main/java/fr/epsi/widgets/transfer/TransferQueue.java
@@ -0,0 +1,6 @@
+package fr.epsi.widgets.transfer;
+
+import javafx.scene.control.TableView;
+
+public class TransferQueue extends TableView {
+}
