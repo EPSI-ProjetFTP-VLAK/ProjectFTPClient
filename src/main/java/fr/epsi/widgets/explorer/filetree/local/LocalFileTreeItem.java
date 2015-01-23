ae14fd3ed43914e567f3be1e7eab9f3042feca48
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2015-01-23 10:11:39 +0100

    LocalFileTreeItem implementation correction

--- src/main/java/fr/epsi/widgets/explorer/filetree/local/LocalFileTreeItem.java
+++ src/main/java/fr/epsi/widgets/explorer/filetree/local/LocalFileTreeItem.java
@@ -3,6 +3,8 @@ package fr.epsi.widgets.explorer.filetree.local;
 import fr.epsi.dto.FileDTO;
 import fr.epsi.widgets.explorer.filetree.FileTreeItem;
 
+import java.io.File;
+
 public class LocalFileTreeItem extends FileTreeItem {
 
     public LocalFileTreeItem(FileDTO file) {
@@ -18,8 +20,8 @@ public class LocalFileTreeItem extends FileTreeItem {
         this.getChildren().clear();
 
         try {
-            for (FileDTO subFile : file.getChildren()) {
-                FileTreeItem treeNode = new LocalFileTreeItem(subFile);
+            for (File subFile : file.getFile().listFiles()) {
+                FileTreeItem treeNode = new LocalFileTreeItem(new FileDTO(subFile));
                 this.getChildren().add(treeNode);
             }
         } catch (NullPointerException e) {
